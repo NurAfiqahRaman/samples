78cdc81d98dbc2b09030abb76f3251b6482dd59f
NurAfiqahRaman <80899173+NurAfiqahRaman@users.noreply.github.com>
2021-03-18 23:12:50 +0800

    Add files via upload

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,19 @@
+package samples.demo;
+
+public class Location {
+	private int id;
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+
+	
+	
+}
